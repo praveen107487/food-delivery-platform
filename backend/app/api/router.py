2f1b15a836733106eda02192baf134c336e7188e
Praveen N <praveen107487@gmail.com>
2026-07-23 19:44:30 +0530

    feat(customer): add customer router to API

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 from app.auth.router import router as auth_router
 from app.cart.router import router as cart_router
+from app.customer.router import router as customer_router
 from app.restaurant.router import (
     menu_item_router,
     restaurant_router,
@@ -13,3 +14,4 @@ api_router.include_router(auth_router)
 api_router.include_router(restaurant_router)
 api_router.include_router(menu_item_router)
 api_router.include_router(cart_router)
+api_router.include_router(customer_router)
