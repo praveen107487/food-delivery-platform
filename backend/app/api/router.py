cee891dee19024080856af6275f3aa1bc25413c8
Praveen N <praveen107487@gmail.com>
2026-07-27 15:57:58 +0530

    feat(notification): include notification router in API

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from app.auth.router import router as auth_router
 from app.cart.router import router as cart_router
 from app.customer.router import router as customer_router
+from app.notification.router import router as notification_router
 from app.order.router import router as order_router
 from app.payment.router import router as payment_router
 from app.restaurant.router import (
@@ -21,3 +22,4 @@ api_router.include_router(customer_router)
 api_router.include_router(order_router)
 api_router.include_router(payment_router)
 api_router.include_router(review_router)
+api_router.include_router(notification_router)
